9cd7e7d4d8f77235d749e90077874b785ac92533
Dj <148027370+djsy104@users.noreply.github.com>
2025-07-14 15:32:38 -0700

    chore: add Prettier config and update ESLint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import js from '@eslint/js';
+import globals from 'globals';
 import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
-    settings: { react: { version: 'detect' } },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     plugins: {
       react,
       'react-hooks': reactHooks,
@@ -32,6 +36,7 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'no-unused-vars': 'warn', //this changes the error to a warning
       'react/prop-types': 'off', //this suppresses warnings about not using prop-types
+      //potential outdated instructions mentioned by mentor? unsure of how to fix
     },
   },
-])
+]);
