4a6a62b00b1189b086a720284eda126bf48a2b1a
Dj <148027370+djsy104@users.noreply.github.com>
2025-08-18 00:02:02 -0700

    feat: allow user to edit todo title and refactored
    - Refactored input and label into re-usable component
    - Refactored TodoForm to use re-usable component

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,13 +1,52 @@
-function TodoListItem({ todo, onCompleteTodo }) {
+import { useState } from 'react';
+import TextInputWithLabels from '../../shared/TextInputWithLabel';
+
+function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(todo.title);
+
+  function handleCancel() {
+    setWorkingTitle(todo.title);
+    setIsEditing(false);
+  }
+
+  function handleEdit(event) {
+    setWorkingTitle(event.target.value);
+  }
+
+  function handleUpdate(event) {
+    if (!isEditing) return;
+
+    event.preventDefault();
+    onUpdateTodo({ ...todo, title: workingTitle });
+    setIsEditing(false);
+  }
   return (
     <li>
-      <form>
-        <input
-          type="checkbox"
-          checked={todo.isCompleted}
-          onChange={() => onCompleteTodo(todo.id)}
-        ></input>
-        {todo.title}
+      <form onSubmit={handleUpdate}>
+        {isEditing ? (
+          <>
+            <TextInputWithLabels
+              value={workingTitle}
+              onChange={(e) => handleEdit(e)}
+            />
+            <button type="button" onClick={() => handleCancel()}>
+              Cancel
+            </button>
+            <button type="button" onClick={(e) => handleUpdate(e)}>
+              Update
+            </button>
+          </>
+        ) : (
+          <>
+            <input
+              type="checkbox"
+              checked={todo.isCompleted}
+              onChange={() => onCompleteTodo(todo.id)}
+            ></input>
+            <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+          </>
+        )}
       </form>
     </li>
   );
